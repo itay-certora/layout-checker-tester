19f75ccd8e73b184d2863cc51fda48ac9c7db1be
itay.h <itay.h@certora.com>
2025-07-28 15:41:37 +0300

    initial commit again

--- src/Test.sol
+++ src/Test.sol
@@ -18,20 +18,6 @@ contract Test {
     mapping(address => bool) map;
     uint128[5] five;
     bool b0;
+    bool b1;
     bool[4][2] bools;
-}
-
-contract Test2 {
-    bytes29 _b;
-    uint128 a;
-    uint64 b;
-    uint128 u;
-}
-
-contract Test3 {
-    bytes30 _b;
-    uint64 a;
-    uint128 b;
-    uint128 u;
-    uint64 uu;
 }
