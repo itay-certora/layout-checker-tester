067bb618de027e6f1c5ef4a079ec527678291b38
itay.h <itay.h@certora.com>
2025-07-29 11:04:23 +0300

    Add inheritance

---
+++ src/Kaki.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract Kaki {
+    uint public number;
+    string public name;
+    bool public isActive;
+ 
+}
--- src/Test.sol
+++ src/Test.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
-
-contract Test {
+import {Kaki} from "./Kaki.sol";
+contract Test is Kaki{
     enum E {
         ONE,
         TWO
